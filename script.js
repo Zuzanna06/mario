0c404da54b823295d38eda662f54951deb880f03
Zuzanna06 <103110190+Zuzanna06@users.noreply.github.com>
2023-03-24 15:38:24 +0100

    deuxeme push

--- script.js
+++ script.js
@@ -75,6 +75,29 @@
  
 //  // une fonction avec le mot clé function est accessible depuis l'extérieur de l'objet tandis qu'une fonction fléchée est cantonnée à l'intérieur de l'objet
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+
+
+//When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
@@ -139,9 +162,11 @@ console.log(boutons);
 for (let index = 0; index < boutons.length; index++) {
     console.log(boutons[index])
     boutons[index].addEventListener('click', e => {
-        console.log(e);
+        console.log(boutons[index].id);
+        let key = boutons[index].id; // je stock chaques button avec leurs id
+        console.log(maCollestion[key]); 
+        modal.style.display = "block";
     })
 }
-
 
 
